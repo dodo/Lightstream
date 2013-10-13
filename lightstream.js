ef893d2cc79c49cb5f02356df0905bb88c37bf37
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2013-10-13 07:11:27 +0200

    new lightstream architecture

--- lightstream.js
+++ lightstream.js
@@ -1,69 +1,32 @@
-'use strict';
+var extend = require('extend');
+var Router = require('./lib/router').Router;
 
-~function () {
-    var ltxXPath = "require('ltx-xpath')"; // FIXME
+exports.xmpp = {
+    Presence: function () {throw Error "no backend loaded!"},
+    Message: function () {throw Error "no backend loaded!"},
+    Stanza: function () {throw Error "no backend loaded!"},
+    JID: function () {throw Error "no backend loaded!"},
+    Iq: function () {throw Error "no backend loaded!"},
+};
 
-/* Lightstream xmpp framework */
+exports.Lightstream = Lightstream;
+function Lightstream(options) {
+    options = options || {};
+    this.backend = options.backend;
+    this.xmpp = extend({}, exports.xmpp);
+    this.connection = this.backend.connection;
+    this.router = new Router(this, options.timeout);
+    this.extension = {};
+}
 
-window.Lightstream = {};
+Lightstream.prototype.send = function (stanza) {
+    this.backend.send(stanza);
+};
 
-// config
-Lightstream.timeout = 1000; // does a second make sense?
-
-Lightstream.Router = function (con) {
-    this.connection = con;
-    this.xpath = new ltxXPath();
-    // listen for events
-    if (con.onstanza || con.onstatechange) {
-        throw new Error("please provide unused lightstring connection.");
+Lightstream.prototype.registerExtension = function (name, extension) {
+    if (this.extension[name]) {
+        console.warn("Extension '%s' exists allready.", name);
+        return this;
     }
-    con.onstatechange = this.onstatechange.bind(this);
-    con.onstanza = this.onstanza.bind(this);
+    return (this.extension[name] = extension);
 };
-
-Lightstream.Router.prototype = new EventEmitter();
-
-Lightstream.Router.prototype.match = function (xpath, namespaces, callback) {
-    this.xpath.on(xpath, namespaces, callback);
-    return this;
-};
-
-Lightstream.Router.prototype.send = function (stanza) {
-    this.connection.send(stanza);
-    return this;
-};
-
-Lightstream.Router.prototype.request = function (xpath, namespaces, callback) {
-    // TODO autogenerate xpath from stanza to be send
-    var that = this;
-    if (!callback) {
-        callback = namespaces;
-        namespaces = undefined;
-    }
-    var timeout = setTimeout(function () {
-        that.xpath.removeListener(xpath, response);
-        if (callback) callback("timeout");
-    }, Lightstream.timeout);
-    this.xpath.once(xpath, namespaces, function response(stanza) {
-        clearTimeout(timeout);
-        if (callback) callback(null, stanza);
-    });
-    return this;
-};
-
-// connection handlers
-
-Lightstream.prototype.onstanza = function (stanza) {
-    // dispatch stanza to callback or handle error
-    if (!this.xpath.match(stanza)) {
-        this.emit('error', "unhandled stanza", stanza);
-    }
-};
-
-Lightstream.Router.prototype.onstatechange = function (state) {
-    // supose the state is a string
-    this.emit(state);
-};
-
-
-}();
